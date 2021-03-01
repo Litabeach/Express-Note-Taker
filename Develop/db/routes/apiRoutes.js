0d0f0241a3abb7c5fa1f15c40d36f88cf6442726
Lita Beach <litabeach123@gmail.com>
2021-03-01 13:59:54 -0600

    fixed bug in POST by using UUID

--- Develop/db/routes/apiRoutes.js
+++ Develop/db/routes/apiRoutes.js
@@ -1,4 +1,8 @@
 const noteData = require("../db");
+const fs = require("fs");
+const path = require("path");
+const uuid = require("uuid");
+
 
 module.exports = function (app) {
     //GET /api/notes should read the db.json file and return all saved notes as JSON.
@@ -7,13 +11,19 @@ module.exports = function (app) {
     });
 
     //POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
-    app.post("/api/notes", function (req, res) {
-        // console.log(req.body)
+
+    app.post('/api/notes', (req, res) => {
         const newNote = req.body;
-        //push the new note to array of notes
+        //set unique IDs using the UUID module
+        newNote.id = uuid.v4();
         noteData.push(newNote);
+        fs.writeFileSync(
+          path.join(__dirname, 'db.json'),
+          JSON.stringify(noteData)
+        );
         res.json(newNote);
-    });
+      });
+
 
     // DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
     app.delete('/api/notes/:id', (req, res) => {
@@ -23,9 +33,9 @@ module.exports = function (app) {
       
         //assign the noteData ids
         for(let i = 0; i < noteData.length; i++){
-          console.log(noteData[i]);
+        //   console.log(noteData[i]);
           noteData[i].id = i;
-          console.log(noteData[i]);
+        //   console.log(noteData[i]);
         }
         
         //update the db.json file
