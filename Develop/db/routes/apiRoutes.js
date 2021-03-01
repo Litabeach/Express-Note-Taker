b247bf9bb4fa421d5563cb12d6e3ca18bed0e636
Lita Beach <litabeach123@gmail.com>
2021-02-28 19:10:18 -0600

    working on the delete api route

--- Develop/db/routes/apiRoutes.js
+++ Develop/db/routes/apiRoutes.js
@@ -1,5 +1,5 @@
 const noteData = require("../db");
-const fs = require("fs")
+// const fs = require("fs")
 
 
 module.exports = function (app) {
@@ -9,17 +9,25 @@ module.exports = function (app) {
     });
 
     //post 
+    //POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
     app.post("/api/notes", function (req, res) {
         noteData.push(req.body);
         res.json(true);
 
     });
 
-     //delete 
-     app.delete("/api/notes", function (req, res) {
-        noteData.empty(req.body)
-        // res.json(true);
+    //delete
+    // DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 
+    app.delete("/api/notes:id", function (req, res) {
+        const deleteID = req.params.id;
+        for (let i = 0; i < noteData.length; i++) {
+            let note = noteData[i];
+            if (note.id === deleteID) {
+                noteData.empty();
+            }
+        }
+        
     });
 
 };
