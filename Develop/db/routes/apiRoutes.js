39cb8b4e8a58d6a7a6d41a4e50869deced778a0d
Lita Beach <litabeach123@gmail.com>
2021-03-01 15:14:38 -0600

    added fs write file to delete api route so that the db.json file is re written when deleted take place

--- Develop/db/routes/apiRoutes.js
+++ Develop/db/routes/apiRoutes.js
@@ -26,12 +26,17 @@ module.exports = function (app) {
 
   // DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
   app.delete("/api/notes/:id", function(req, res) {
-    const deleteNote = req.params.id;
+    const deleteNoteID = req.params.id;
     for (let i = 0; i < noteData.length; i++) {
-        if (noteData[i].id === deleteNote) {
+        if (noteData[i].id === deleteNoteID) {
             noteData.splice(i, 1);
         }
     }
+    //write the file 
+    fs.writeFileSync(
+      path.join(__dirname, '../db.json'),
+      JSON.stringify(noteData)
+    );
     //update the db.json file
     res.json(req.body);
   });
