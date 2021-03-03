b4dadadb6922e2e918d081714adf441a827973c6
Lita Beach <litabeach123@gmail.com>
2021-03-02 22:15:09 -0600

    added a / route to htmlRoutes for heroku

--- Develop/db/routes/htmlRoutes.js
+++ Develop/db/routes/htmlRoutes.js
@@ -3,6 +3,11 @@ var path = require("path");
 
 module.exports = function (app) {
 
+    //for heroku
+    app.get("/", function(req, res) {
+        res.json(path.join(__dirname, "public/index.html"));
+      });
+
     //route for notes.html
     app.get("/notes", function (req, res) {
         res.sendFile(path.join(__dirname, "../../public/notes.html"));
