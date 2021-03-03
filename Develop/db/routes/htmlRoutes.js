b37d26ad4ff64f83c6180e42079f47e852430061
Lita Beach <litabeach123@gmail.com>
2021-03-02 22:29:47 -0600

    updated htmlRoutes, removed wildcard replaced with slash

--- Develop/db/routes/htmlRoutes.js
+++ Develop/db/routes/htmlRoutes.js
@@ -3,20 +3,14 @@ var path = require("path");
 
 module.exports = function (app) {
 
-    //for heroku
-    app.get("/", function(req, res) {
-        res.json(path.join(__dirname, "public/index.html"));
-      });
-
     //route for notes.html
     app.get("/notes", function (req, res) {
         res.sendFile(path.join(__dirname, "../../public/notes.html"));
-
-        
     });
 
-    // If no matching route is found default to home
-    app.get("*", function (req, res) {
+    // If no matching route is found default to home 
+    //changed "*" to "/" for heroku deployment
+    app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "../../public/index.html"));
     });
 
